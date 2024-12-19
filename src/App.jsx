f358385a80d22bd79cb4829ee31734a3fbd02e87
chetan panara <chetanpanara88@gmail.com>
2024-12-19 12:28:47 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BrowserRouter, Routes ,Route} from "react-router-dom";
 import Home from "./components/Home";
+import Signup from "./components/Signup";
 
 
 const App = () => {
@@ -13,7 +14,8 @@ const App = () => {
          <Navbar />
         <Routes>
 
-            <Route path="/" element={<Home/>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Signup" element={<Signup/>}></Route>
           
           </Routes>
        <Footer />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,11 @@ import {
   PopoverButton,
   PopoverGroup,
   PopoverPanel,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
 } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon, ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline'
 import { navigation } from "../constants/Constants";
