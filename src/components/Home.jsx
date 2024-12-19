abac1066652f54b8e4ba66a84348803fc88a983e
shyam-213 <shyamvaland72@gmail.com>
2024-12-19 12:07:38 +0530

    changes

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import FeatureSection from './FeatureSection'
+import Pricing from './Pricing'
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection/>
+      <FeatureSection/>
+      <Pricing/>
+    </div>
+  )
+}
+
+export default Home
