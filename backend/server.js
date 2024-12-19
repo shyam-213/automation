8c95f313f97333da12a09a759268013adce143d8
shyam-213 <shyamvaland72@gmail.com>
2024-12-19 17:12:41 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,32 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
+mongoose
+  .connect("mongodb://0.0.0.0:27017/ecommerce")
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect("mongodb+srv://chetan:chetan88@selkey.uvauy.mongodb.net/?retryWrites=true&w=majority&appName=selkey")
-  .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.log(error));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
