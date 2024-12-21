d0613cf3b60345fe1d0cfc8526f154866daf5bb3
chetan panara <chetanpanara88@gmail.com>
2024-12-21 13:58:10 +0530

    descarded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import Navbar from "./components/common/Navbar";
+
 import Footer from "./components/common/Footer";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Signin from "./pages/auth/Signin";
 import Signup from "./pages/auth/Signup";
 import ForgotPassword from "./pages/auth/forgotPassword";
 import Pricing from "./pages/Pricing";
+import { useState } from "react";
+import UserNavbar from "./UserDashboard/Common/UserNavbar";
 
 
 const App = () => {
+
+  const [isLogin, setisLogin] = useState(true)
+
   return (
     <>
       <BrowserRouter>
@@ -19,10 +25,15 @@ const App = () => {
           <Route path="/Signup" element={<Signup />}></Route>
           <Route path="/Signin" element={<Signin />}></Route>
           <Route path="/forgotPassword" element={<ForgotPassword />}></Route>
-          
           <Route path="/pricing" element={<Pricing />}></Route>
-        </Routes>
-        <Footer />
+            </Routes>
+            </>
+            
+            : <UserNavbar />
+        }
+       
+        
+        {/* <Footer /> */}
       </BrowserRouter>
     </>
   );
