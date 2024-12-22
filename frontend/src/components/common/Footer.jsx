46127d070bc104490bd2b8b61d8ad3e45d7fc19f
chetan panara <chetanpanara88@gmail.com>
2024-12-22 21:03:04 +0530

    commit

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -120,18 +120,18 @@ const Footer = () => {
 
       <footer class="bg-white dark:bg-gray-900">
         <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-          <div class="md:flex md:justify-between">
 
-            <div class="mb-6 md:mb-0 max-lg:w-full w-2/6">
+          <div class="lg:flex md:justify-between">
+            <div class="mb-6 md:mb-0 max-lg:w-full w-2/5">
               <a href={'/'} class="flex items-center mb-4">
                 <img src="./img/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Selkey Automation</span>
               </a>
-              <span>Selkey Automation, <br />
+              <span className="">Selkey Automation, <br />
                 Anand, Gujarat, India</span>
             </div>
 
-            <div class="grid grid-cols-2 lg:grid-cols-4 gap-8 sm:gap-6 sm:grid-cols-2">
+            <div class="grid grid-cols-2 lg:grid-cols-4 gap-8 sm:gap-6 my-6 lg:mt-0 sm:grid-cols-2 md:grid-cols-3">
 
 
               <div>
@@ -190,7 +190,8 @@ const Footer = () => {
 
             </div>
           </div>
-          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+
+          <hr class="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-2" />
 
           <div class="sm:flex sm:items-center sm:justify-between">
             <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Selkey</a>. All Rights Reserved.
